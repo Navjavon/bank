213add58c6037bca4a0c117eac8858a4ad42c1dc
Navjavon Kholiqov <navin.kholikov@gmail.com>
2020-09-29 11:39:42 +0500

    fixed types

--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -3,40 +3,12 @@ package types
 // Money represents a monetary amount in minimum units (cents, kopecks, diramas, etc.).
 type Money int64
 
-// Currency presents currency code.
-type Currency string
-
-// Currency codes
-const (
-	TJS Currency = "TJS"
-	RUB Currency = "RUB"
-	USD Currency = "USD"
-)
-
-// PAN presents number of card.
-type PAN string
-
-// Card presents information about card.
-type Card struct {
-	ID       int
-	PAN      PAN
-	Balance  Money
-	MinBalance Money
-	Currency Currency
-	Color    string
-	Name     string
-	Active   bool
-}
+// Categoty represents the category in which the payment was made (cars, pharmacies, restaurants, etc.).
+type Categoty string
 
 // Payment presents information about the payment.
 type Payment struct {
-	ID int
-	Amount Money
-}
-
-// PaymentSource presents source of payments.
-type PaymentSource struct {
-	Type string
-	Number string
-	Balance Money
+	ID       int
+	Amount   Money
+	Categoty Categoty
 }
